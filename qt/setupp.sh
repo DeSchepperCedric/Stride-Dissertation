67954666ab66f8b2ba585723959cc015c7e5a137
Unknown <wannes.marynen@student.uantwerpen.be>
2019-03-25 11:17:41 +0100

    demo
    run ./qt/setup.sh

---
+++ qt/setupp.sh
@@ -0,0 +1,4 @@
+sudo apt install qtdeclarative5-dev
+sudo apt install qtbase5-dev 
+sudo apt install qtlocation5-dev qtpositioning5-dev 
+sudo apt install qml-module-qtlocation qml-module-qtpositioning
