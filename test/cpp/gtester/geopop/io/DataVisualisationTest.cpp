a78eb11171c0d372dbab7772d137d1387caddc98
Unknown <wannes.marynen@student.uantwerpen.be>
2019-03-31 23:22:35 +0200

    added dummy Test

---
+++ test/cpp/gtester/geopop/io/DataVisualisationTest.cpp
@@ -0,0 +1,19 @@
+//
+// Created by wannes on 31.03.19.
+//
+
+#include <gtest/gtest.h>
+
+using namespace std;
+
+namespace {
+
+    TEST(DataVisualisationTest, readerTest){
+        EXPECT_TRUE(true);
+    }
+
+    TEST(DataVisualisationTest, writerTest){
+        EXPECT_TRUE(true);
+    }
+
+}
