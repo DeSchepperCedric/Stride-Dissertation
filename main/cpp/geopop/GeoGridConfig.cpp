7cd3efa310e2ecd389f824dbac24991bbd618db9
Cedric De Schepper <cedric.deschepper99@gmail.com>
2019-05-22 14:14:08 -0700

    fix commute test

--- main/cpp/geopop/GeoGridConfig.cpp
+++ main/cpp/geopop/GeoGridConfig.cpp
@@ -37,21 +37,21 @@ GeoGridConfig::GeoGridConfig() : param{}, refHH{}, info{} {}
 
 GeoGridConfig::GeoGridConfig(const ptree& configPt) : GeoGridConfig()
 {
-        const auto pt = configPt.get_child("run.geopop_gen");
+        const auto pt                      = configPt.get_child("run.geopop_gen");
         param.pop_size                     = pt.get<unsigned int>("population_size");
         param.participation_college        = pt.get<double>("participation_college");
         param.fraction_workplace_commuters = pt.get<double>("fraction_workplace_commuters");
         param.fraction_college_commuters   = pt.get<double>("fraction_college_commuters");
         param.particpation_workplace       = pt.get<double>("particpation_workplace");
 
-        people[Id::K12School]              = pt.get<unsigned int>("people_per_K12School", 500U);
-        people[Id::College]                = pt.get<unsigned int>("people_per_College", 3000U);
-        people[Id::Workplace]              = pt.get<unsigned int>("people_per_Workplace", 20U);
-        people[Id::PrimaryCommunity]       = pt.get<unsigned int>("people_per_PrimaryCommunity", 2000U);
-        people[Id::SecondaryCommunity]     = pt.get<unsigned int>("people_per_SecondaryCommunity", 2000U);
+        people[Id::K12School]          = pt.get<unsigned int>("people_per_K12School", 500U);
+        people[Id::College]            = pt.get<unsigned int>("people_per_College", 3000U);
+        people[Id::Workplace]          = pt.get<unsigned int>("people_per_Workplace", 20U);
+        people[Id::PrimaryCommunity]   = pt.get<unsigned int>("people_per_PrimaryCommunity", 2000U);
+        people[Id::SecondaryCommunity] = pt.get<unsigned int>("people_per_SecondaryCommunity", 2000U);
 
-        pools[Id::K12School]              = pt.get<unsigned int>("pools_per_K12School", 25U);
-        pools[Id::College]                = pt.get<unsigned int>("pools_per_College", 20U);
+        pools[Id::K12School] = pt.get<unsigned int>("pools_per_K12School", 25U);
+        pools[Id::College]   = pt.get<unsigned int>("pools_per_College", 20U);
 }
 
 void GeoGridConfig::SetData(const string& householdsFileName)
